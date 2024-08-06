e5f74b38de927f2a247cbcb527e1ea219dbc2a7a
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2024-08-06 11:43:40 +0100

    Changes for Github Pages

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,28 @@
 import './index.css'
+import mapIcon from './assets/map-icon.svg'
+import mountFuji from './assets/MountFuji.png'
+import sydneyOperaHouse from './assets/SydneyOperaHouse.png'
+import geirangerfjord from './assets/Geirangerfjord.png'
 
 export default function Card(props) {
+    let image
+    if(props.imageUrl === 'MountFuji.png') {
+        image = mountFuji
+    } else if(props.imageUrl === 'SydneyOperaHouse.png') {
+        image = sydneyOperaHouse
+    } else {
+        image = geirangerfjord
+    }
     return (
         <>
             <section className="card">
                 <img
-                    src={`./public/${props.imageUrl}`}
+                    src={image}
                     className="card-image"
                 />
                 <div className="card-content">
                     <div className="location-container">
-                        <img src="./src/assets/map-icon.svg" alt="map icon" />
+                        <img src={mapIcon} alt="map icon" />
                         <p>{props.location}</p>
                         <a href={props.googleMapsUrl} target='_blank'>View on Google Maps</a>
                     </div>
